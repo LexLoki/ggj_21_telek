c8981316b77d8038beeb0530f0b415f70f680532
arthusagain <arthus.james@hotmail.com>
2021-01-30 00:44:30 -0300

    teste de projetil

---
+++ Assets/Scripts/Atirador.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Atirador : MonoBehaviour
+{
+    public GameObject projetil;
+    public float intervalo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(EsperaTiro());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator EsperaTiro()
+    {
+        yield return new WaitForSeconds(intervalo);
+        Debug.Log("Fogo!");
+        Instantiate(projetil,transform.position,Quaternion.identity);
+        StartCoroutine(EsperaTiro());
+    }
+}
