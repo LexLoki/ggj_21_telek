1eb7dd198a6694c1a13be0cfc8e11e02001a3cb6
mlhomsi <mlhomsi@gmail.com>
2021-01-30 10:58:14 -0300

    Parallax Teste

---
+++ Assets/Scripts/Camera/Parallax/ParaBackground.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+ [ExecuteInEditMode]
+ public class ParaBackground : MonoBehaviour
+ {
+   public ParaCamera paraCamera;
+   List<ParaLayer> paraLayers = new List<ParaLayer>();
+  
+   void Start()
+   {
+       if (paraCamera == null)
+         paraCamera = Camera.main.GetComponent<ParaCamera>();
+       if (paraCamera != null)
+         paraCamera.onCameraTranslate += Move;
+       SetLayers();
+   }
+  
+   void SetLayers()
+   {
+       paraLayers.Clear();
+       for (int i = 0; i < transform.childCount; i++)
+       {
+           ParaLayer layer = transform.GetChild(i).GetComponent<ParaLayer>();
+  
+           if (layer != null)
+           {
+               layer.name = "Layer-" + i;
+               paraLayers.Add(layer);
+           }
+       }
+     }
+     void Move(float delta)
+     {
+         foreach (ParaLayer layer in paraLayers)
+       {
+           layer.Move(delta);
+       }
+   }
+ }
---
+++ Assets/Scripts/Camera/Parallax/ParaLayer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+
+public class ParaLayer : MonoBehaviour
+ {
+       public float parallaxFactor;
+       public void Move(float delta)
+       {
+           Vector3 newPos = transform.localPosition;
+           newPos.x -= delta * parallaxFactor;
+           transform.localPosition = newPos;
+       }
+ }
