36f527527641efa94fc05cf1a4005ac39879564d
arthusagain <arthus.james@hotmail.com>
2021-01-30 19:37:13 -0300

    atiradores recebem rotação inicial do projetil

--- Assets/Scripts/Atirador.cs
+++ Assets/Scripts/Atirador.cs
@@ -6,6 +6,7 @@ public class Atirador : MonoBehaviour
 {
     public GameObject projetil;
     public float intervalo;
+    public Vector3 rotacaoProjetil;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,7 +22,10 @@ public class Atirador : MonoBehaviour
     IEnumerator EsperaTiro()
     {
         yield return new WaitForSeconds(intervalo);
-        Instantiate(projetil,transform.position,Quaternion.identity);
+        Object projectile = Instantiate(projetil,transform.position,Quaternion.Euler(rotacaoProjetil.x,rotacaoProjetil.y,rotacaoProjetil.z));
+
+        
+
         StartCoroutine(EsperaTiro());
     }
 }
