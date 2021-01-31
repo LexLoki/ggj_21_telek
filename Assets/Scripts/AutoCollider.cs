4535711e42dc89074403f1c9645d2e506f893e3a
mlhomsi <mlhomsi@gmail.com>
2021-01-31 14:57:52 -0300

    Fase 01

---
+++ Assets/Scripts/AutoCollider.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoCollider : MonoBehaviour
+{
+    // Start is called before the first frame update
+   void Start()
+    {
+        this.gameObject.AddComponent<PolygonCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
