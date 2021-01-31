75b0bfeb0877d6d1b8adbca65af740c357cd5dcd
arthusagain <arthus.james@hotmail.com>
2021-01-31 14:17:57 -0300

    scripts de objeto quebrável e elevador (ou plataforma movel de 1 direçao)

---
+++ Assets/Scripts/Objetos moveis/Breakable.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Breakable : MonoBehaviour
+{
+    public int health = 1;
+    // Start is called before the first frame update
+    
+    void OnCollisionEnter2D(Collider2D outro)
+    {
+        if(outro.tag == "Projetil")
+        {
+            health -= 1;
+            if (health <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
