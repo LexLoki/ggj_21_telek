a509ca69b7a1d9a7affe398b49840314141437b8
arthusagain <arthus.james@hotmail.com>
2021-02-26 15:46:43 -0300

    elevador pode ser ativado por toque

--- Assets/Scripts/Objetos moveis/MoveTo.cs
+++ Assets/Scripts/Objetos moveis/MoveTo.cs
@@ -6,16 +6,35 @@ public class MoveTo : MonoBehaviour
 {
     public float velocidade;
     public Vector3 destino;
+    public bool AtivadoPorToque;
+    private bool ativo;
     // Start is called before the first frame update
     void Start()
     {
-
+        if (AtivadoPorToque)
+        {
+            ativo = false;
+        }
+        else ativo = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position,destino,velocidade * Time.deltaTime);
+        if (ativo)
+        {
+            transform.position = Vector3.MoveTowards(transform.position,destino,velocidade * Time.deltaTime);
+
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        //if(other.tag != "Botao")
+        if (other.collider.tag == "Player")
+        {
+            ativo = true;
+        }
     }
 
     
